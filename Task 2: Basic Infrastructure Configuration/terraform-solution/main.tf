4f69632e28522cdcd808000ee7d0d83e2f6d98f3
Dmitriy Dovguchev <dzmitry.douhushau@softnetix.io>
2025-06-22 00:25:00 +0300

    add: acl vpc

--- Task 2: Basic Infrastructure Configuration/terraform-solution/main.tf
+++ Task 2: Basic Infrastructure Configuration/terraform-solution/main.tf
@@ -49,3 +49,38 @@ resource "aws_route_table_association" "private" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_network_acl" "network-acl" {
+  vpc_id = module.vpc_creator.vpc.id
+
+  tags = {
+    Name = "network-acl-nacl"
+  }
+}
+
+resource "aws_network_acl_rule" "allow_all_inbound" {
+  network_acl_id = aws_network_acl.network-acl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+}
+
+resource "aws_network_acl_rule" "allow_all_outbound" {
+  network_acl_id = aws_network_acl.network-acl.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+}
+
+resource "aws_network_acl_association" "assoc" {
+  subnet_id      = module.vpc_creator.public_subnets.public_1.id
+  network_acl_id = aws_network_acl.network-acl.id
+}
